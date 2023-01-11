cf7f46035157f2ef40cd27196df39d201a1d055b
Nathan Kamenchu <kamenchunathan@gmail.com>
2023-01-11 21:44:43 +0300

    Add graphql Queries for Polls

--- django-api/graphql_api/schema.py
+++ django-api/graphql_api/schema.py
@@ -1,19 +1,69 @@
-import graphene 
+from typing import Iterable
+import graphene
 from graphene_django import DjangoObjectType
-from polls.models import Poll
+from polls import models
+from polls.models import (
+    Poll as PollModel,
+    PollCharField as PollCharFieldModel,
+    PollChoiceField as PollChoiceFieldModel,
+    PollMultiChoiceField as PollMultiChoiceFieldModel,
+    PollTextField as PollTextFieldModel
+)
 
-
-class PollType(DjangoObjectType):
+class PollCharField(DjangoObjectType):
     class Meta:
-        model = Poll
+        model = PollCharFieldModel
+        exclude = ('poll',)
+ 
+ 
+class PollTextField(DjangoObjectType):
+    class Meta:
+        model =PollTextFieldModel
+        exclude = ('poll',)
+
+
+class PollChoiceField(DjangoObjectType):
+    class Meta:
+        model = PollChoiceFieldModel
+        exclude = ('poll',)
+        
+class PollMultiChoiceField(DjangoObjectType):
+    class Meta:
+        model = PollMultiChoiceFieldModel
+        exclude = ('poll',)
+
+
+class PollField(graphene.Union):
+    class Meta:
+        types = (PollCharField, PollTextField, PollChoiceField, PollMultiChoiceField)
+    
+
+class Poll(DjangoObjectType):
+    class Meta:
+        model = PollModel
         fields = ('title', 'description')
+
+    poll_fields = graphene.List(PollField)
+
+    def resolve_poll_fields(root, info, **kwargs) -> Iterable[PollField]:
+        return [*PollCharFieldModel.objects.filter(poll=root),
+                *PollTextFieldModel.objects.filter(poll=root),
+                *PollChoiceFieldModel.objects.filter(poll=root),
+                *PollMultiChoiceFieldModel.objects.filter(poll=root)
+                ]
+                
+        
 
 
 class RootQuery(graphene.ObjectType):
-    polls = graphene.List(PollType)
+    hello = graphene.String()
+    polls = graphene.List(Poll)
+
+    def resolve_hello(root, info, **kwargs):
+        return 'Hello world'
+
 
     def resolve_polls(root, info, **kwargs):
-        Poll.objects.all()
-
+        return PollModel.objects.all()
 
 schema = graphene.Schema(query=RootQuery)
