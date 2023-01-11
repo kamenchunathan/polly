fd2f76a4de982248861de71cd8bfc16daf31605c
Nathan Kamenchu <kamenchunathan@gmail.com>
2023-01-11 22:53:41 +0300

    Register poll field answer models to admin

--- django-api/polls/admin.py
+++ django-api/polls/admin.py
@@ -6,6 +6,10 @@ from .models import (
     PollChoiceField,
     PollMultiChoiceField,
     PollTextField,
+    PollCharFieldAnswer,
+    PollTextFieldAnswer,
+    PollChoiceFieldAnswer,
+    PollMultiChoiceFieldAnswer
 )
 
 admin.site.register(Poll)
@@ -13,3 +17,7 @@ admin.site.register(PollCharField)
 admin.site.register(PollTextField)
 admin.site.register(PollChoiceField)
 admin.site.register(PollMultiChoiceField)
+admin.site.register(PollCharFieldAnswer)
+admin.site.register(PollTextFieldAnswer)
+admin.site.register(PollChoiceFieldAnswer)
+admin.site.register(PollMultiChoiceFieldAnswer)
