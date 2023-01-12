d335f60f440b4f749aac0b8946bbd85767c1d616
Nathan Kamenchu <kamenchunathan@gmail.com>
2023-01-12 12:56:28 +0300

    Add related names to answer models for nicer queries

--- django-api/polls/models.py
+++ django-api/polls/models.py
@@ -3,74 +3,93 @@ from django.contrib.postgres.fields import ArrayField
 from authentication.models import User
 
 
-
 class Poll(models.Model):
     title = models.CharField('Poll title', max_length=100)
-    description = models.CharField( 'Description', max_length=255, blank=True)
+    description = models.CharField('Description', max_length=255, blank=True)
 
 
 class PollCharField(models.Model):
-    question_text = models.CharField( 'Question Text', max_length=100)
-    poll = models.ForeignKey( Poll, on_delete=models.CASCADE)
+    text = models.CharField('Question Text', max_length=100)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
 
 
 class PollCharFieldAnswer(models.Model):
     answer = models.CharField('Answer', max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    field = models.ForeignKey(PollCharField, on_delete=models.CASCADE)
-    
+    field = models.ForeignKey(
+        PollCharField,
+        on_delete=models.CASCADE,
+        related_name='char_field_answers',
+        related_query_name='char_field_answer'
+    )
+
     class Meta:
         unique_together = ('user', 'field')
 
 
 class PollTextField(models.Model):
-    question_text = models.CharField( 'Question Text', max_length=100)
+    text = models.CharField('Question Text', max_length=100)
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
 
 
 class PollTextFieldAnswer(models.Model):
     answer = models.TextField('Answer')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    field = models.ForeignKey(PollTextField, on_delete=models.CASCADE)
-    
+    field = models.ForeignKey(
+        PollTextField,
+        on_delete=models.CASCADE,
+        related_name='text_field_answers',
+        related_query_name='text_field_answer'
+    )
+
     class Meta:
         unique_together = ('user', 'field')
 
 
 class PollChoiceField(models.Model):
-    question_text = models.CharField('Question Text', max_length=100)
-    choices = ArrayField( models.CharField('Choice', max_length=100))
+    text = models.CharField('Question Text', max_length=100)
+    choices = ArrayField(models.CharField('Choice', max_length=100))
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
 
-    
+
 class PollChoiceFieldAnswer(models.Model):
     selected_choice = models.CharField('Selected Choice', max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    field = models.ForeignKey(PollChoiceField, on_delete=models.CASCADE)
-    
+    field = models.ForeignKey(
+        PollChoiceField,
+        on_delete=models.CASCADE,
+        related_name='choice_field_answers',
+        related_query_name='choice_field_answer'
+    )
+
     class Meta:
         unique_together = ('user', 'field')
 
     def save(self, *args, **kwargs):
         if self.selected_choice not in self.field.choices:
             return
-        
+
         super().save(*args, **kwargs)
 
 
-
-
 class PollMultiChoiceField(models.Model):
-    question_text = models.CharField( 'Question Text', max_length=100)
-    choices = ArrayField( models.CharField('Choice', max_length=100))
+    text = models.CharField('Question Text', max_length=100)
+    choices = ArrayField(models.CharField('Choice', max_length=100))
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
 
 
 class PollMultiChoiceFieldAnswer(models.Model):
-    selected_choices = ArrayField(models.CharField('Selected Choices', max_length=100))
+    selected_choices = ArrayField(
+        models.CharField('Selected Choices', max_length=100))
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    field = models.ForeignKey(PollMultiChoiceField, on_delete=models.CASCADE)
-    
+    field = models.ForeignKey(
+        PollMultiChoiceField,
+        on_delete=models.CASCADE,
+        related_name='multichoice_field_answers',
+        related_query_name='multichoice_field_answer'
+
+    )
+
     class Meta:
         unique_together = ('user', 'field')
 
@@ -78,8 +97,5 @@ class PollMultiChoiceFieldAnswer(models.Model):
         for choice in self.selected_choices:
             if choice not in self.field.choices:
                 return
-        
+
         super().save(*args, **kwargs)
-
-
-
