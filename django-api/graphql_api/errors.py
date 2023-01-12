6814be431a60b8b79a588ed0e28856854499251e
Nathan Kamenchu <kamenchunathan@gmail.com>
2023-01-12 12:54:41 +0300

    Add custom GraphQLError to format errors as fields

---
+++ django-api/graphql_api/errors.py
@@ -0,0 +1,18 @@
+import graphene
+from graphene_django.utils import camelize
+
+
+class GraphqlError(graphene.Scalar):
+    @staticmethod
+    def serialize(errors):
+        print(errors)
+        if not isinstance(errors, dict):
+            if isinstance(errors, list) and len(errors) == 0:
+                errors = {}
+            else:
+                errors = {'detail': errors}
+
+        if errors.get('__all__', False):
+            errors['non_field_errors'] = errors.pop('__all__')
+        
+        return camelize(errors)
