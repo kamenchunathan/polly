8f9e04a73ed8f480069608a1455687e289c9a9b3
Nathan Kamenchu <kamenchunathan@gmail.com>
2023-01-12 12:55:39 +0300

    Add forms for all the poll models to be used in graphql schema

---
+++ django-api/polls/forms.py
@@ -0,0 +1,88 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.postgres.forms import SimpleArrayField
+
+
+from .models import (
+    PollCharField,
+    PollChoiceField,
+    PollTextField,
+    PollMultiChoiceField,
+    PollCharFieldAnswer,
+    PollChoiceFieldAnswer,
+    PollMultiChoiceFieldAnswer,
+    PollTextFieldAnswer
+)
+
+
+class PollCharFieldForm(forms.ModelForm):
+    class Meta:
+        model = PollCharField
+        fields = ('text', 'poll')
+
+
+class PollChoiceFieldForm(forms.ModelForm):
+    class Meta:
+        model = PollChoiceField 
+        fields = ('text', 'poll', 'choices')
+    
+    choices = SimpleArrayField(forms.CharField(max_length=100))
+
+
+class PollTextFieldForm(forms.ModelForm):
+    class Meta:
+        model =PollTextField 
+        fields = ('text', 'poll')
+
+
+class PollMultiChoiceFieldForm(forms.ModelForm):
+    class Meta:
+        model = PollMultiChoiceField
+        fields = ('text', 'choices', 'poll')
+
+    choices = SimpleArrayField(forms.CharField(max_length=100))
+
+class PollCharFieldAnswerForm(forms.ModelForm):
+    class Meta:
+        model = PollCharFieldAnswer 
+        fields = '__all__'
+
+
+class PollChoiceFieldAnswerForm(forms.ModelForm):
+    class Meta:
+        model = PollChoiceFieldAnswer
+        fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super().clean()
+        selected_choice = cleaned_data.get('selected_choice')
+        poll_field = cleaned_data.get('field')
+        if selected_choice not in poll_field.choices:
+            raise ValidationError(f'Selected option must be from choices in field: {poll_field.choices}')
+
+
+
+class PollMultiChoiceFieldAnswerForm(forms.ModelForm):
+    class Meta:
+        model = PollMultiChoiceFieldAnswer
+        fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super().clean()
+        selected_choices = cleaned_data.get('selected_choices')
+        poll_field = cleaned_data.get('field')
+
+        if poll_field is None:
+            raise ValidationError('Provide a valid Id for the field')
+
+        for selected_choice in selected_choices:
+            if selected_choice not in poll_field.choices:
+                raise ValidationError(f'Selected option must be from choices in field {poll_field.choices}')
+
+
+
+
+class PollTextFieldAnswerForm(forms.ModelForm):
+    class Meta:
+        model = PollTextFieldAnswer
+        fields = '__all__'
