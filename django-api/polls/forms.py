daaa55f2b5e4bdffe09642a405a6debb51df5f4e
Nathan Kamenchu <kamenchunathan@gmail.com>
2023-04-08 11:31:40 +0300

    format: Format files to look pretty

--- django-api/polls/forms.py
+++ django-api/polls/forms.py
@@ -2,10 +2,15 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.postgres.forms import SimpleArrayField
 
-from .models import (PollCharField, PollChoiceField, PollTextField,
-                     PollMultiChoiceField, PollCharFieldAnswer,
-                     PollChoiceFieldAnswer, PollMultiChoiceFieldAnswer,
-                     PollTextFieldAnswer)
+from .models import (
+    PollCharField,
+    PollChoiceField, PollTextField,
+    PollMultiChoiceField,
+    PollCharFieldAnswer,
+    PollChoiceFieldAnswer,
+    PollMultiChoiceFieldAnswer,
+    PollTextFieldAnswer
+)
 
 
 class PollCharFieldForm(forms.ModelForm):
